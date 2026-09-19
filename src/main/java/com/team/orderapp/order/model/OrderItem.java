d7d07028317550feb1031de39cb7e8509003d1d5
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-09-19 16:14:35 +0900

    feat: order/model의 3가지 클래스를 DB와 1:1 매칭하였습니다.

--- src/main/java/com/team/orderapp/order/model/OrderItem.java
+++ src/main/java/com/team/orderapp/order/model/OrderItem.java
@@ -1,5 +1,25 @@
 package com.team.orderapp.order.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class OrderItem {
-    // TODO: order_item 테이블과 매핑되는 주문 상세 품목(식별자, 주문 ID, 상품 ID, 수량, 결제 단가 등) 도메인 모델 구현
+
+    /*
+     * 작업: order_item 테이블과 매핑되는 주문 상세 품목(식별자, 주문 ID, 상품 ID, 수량, 결제 단가 등) 도메인 모델 구현
+     *
+     * 작업자: 김상진(Dorazee0209)
+     */
+
+    private Long orderItemId;
+    private Long orderId;
+    private Long productId;
+    private Integer quantity;
+
+    // 주문 당시 단가. 이후 상품 가격이 바뀌어도 주문 금액은 유지된다.
+    private BigDecimal unitPrice;
 }
--- src/main/java/com/team/orderapp/order/model/OrderItemUnit.java
+++ src/main/java/com/team/orderapp/order/model/OrderItemUnit.java
@@ -1,5 +1,25 @@
 package com.team.orderapp.order.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class OrderItemUnit {
-    // TODO: order_item_unit 테이블과 매핑되는 주문 품목-시리얼 상품 배정 이력(식별자, 주문품목 ID, 시리얼상품 ID, 배정일시, 반품일시 등) 도메인 모델 구현
+
+    /*
+     * 작업: order_item_unit 테이블과 매핑되는 주문 품목-시리얼 상품 배정 이력(식별자, 주문품목 ID, 시리얼상품 ID, 배정일시, 반품일시 등) 도메인 모델 구현
+     *
+     * 작업자: 김상진(Dorazee0209)
+     */
+
+    private Long orderItemUnitId;
+    private Long orderItemId;
+    private Long productUnitId;
+    private LocalDateTime assignedAt;
+
+    // 반품 전에는 null
+    private LocalDateTime returnedAt;
 }
